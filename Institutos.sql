dd377a22947e58c7ab5dade7c0465e7ca35cba0d
Carlos Emilio Rabazo Márquez <carlos.rabazo@gmail.com>
2019-11-23 08:39:42 +0100

    Create Institutos.sql

---
+++ Institutos.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Institutos(Nombre_Inst CHAR(35), Area CHAR(35), Plazas INTEGER, PRIMARY KEY (Nombre_Inst), UNIQUE (Nombre_Inst));
+CREATE TABLE Estudiantes(ID INTEGER, Nombre_Est CHAR(35), Puntos REAL, Valor INTEGER,PRIMARY KEY ( ID ),UNIQUE ( ID ) );
+CREATE TABLE Solicitudes(ID INTEGER, Nombre_Inst CHAR(35), Via CHAR(35), Decision CHAR(35),PRIMARY KEY ( ID, Nombre_Inst, Via ),
+FOREIGN KEY ( ID ) REFERENCES Estudiantes ( ID ), FOREIGN KEY ( Nombre_Inst ) REFERENCES Institutos ( Nombre_Inst ), UNIQUE ( ID, Nombre_Inst, Via ) );
