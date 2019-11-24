5e3ad56cb627e5724dbfecb1fda412cd28ea9b88
Carlos Emilio Rabazo Márquez <carlos.rabazo@gmail.com>
2019-11-24 22:09:47 +0100

    Poblar_Tablas_Institutos
    Son las queries de inserción de datos de la tabla Institutos y Alumnos

---
+++ Poblar_Tablas_Institutos.sql
@@ -0,0 +1,37 @@
+insert into Institutos values ('Instituto San Isidro', 'Centro', 150); 
+insert into Institutos values ('Instituto Ramiro de Maeztu', 'Salamanca', 360); 
+insert into Institutos values ('Instituto Arturo Soria', 'Hortaleza', 100); 
+insert into Institutos values ('Instituto Torres Quevedo', 'Moncloa', 210);
+
+insert into Estudiantes values (123, 'Antonio', 8.9, 1000); 
+insert into Estudiantes values (234, 'Juan', 8.6, 1500); 
+insert into Estudiantes values (345, 'Isabel', 8.5, 500); 
+insert into Estudiantes values (456, 'Doris', 7.9, 1000); 
+insert into Estudiantes values (567, 'Eduardo', 6.9, 2000); 
+insert into Estudiantes values (678, 'Carmen', 5.8, 200); 
+insert into Estudiantes values (789, 'Isidro', 8.4, 800); 
+insert into Estudiantes values (987, 'Elena', 6.7, 800); 
+insert into Estudiantes values (876, 'Irene', 6.9, 400); 
+insert into Estudiantes values (765, 'Javier', 7.9, 1500); 
+insert into Estudiantes values (654, 'Alfonso', 7.9, 1000); 
+insert into Estudiantes values (543, 'Pedro', 5.4, 2000);
+
+insert into Solicitudes values (123, 'Instituto Ramiro de Maeztu', 'Tecnologia', 'Si'); 
+insert into Solicitudes values (123, 'Instituto Ramiro de Maeztu', 'Ciencias Sociales', 'No');
+insert into Solicitudes values (123, 'Instituto San Isidro', 'Tecnologia', 'Si'); 
+insert into Solicitudes values (123, 'Instituto Torres Quevedo', 'Ciencias Sociales', 'Si'); 
+insert into Solicitudes values (234, 'Instituto San Isidro', 'Ciencias', 'No'); 
+insert into Solicitudes values (345, 'Instituto Arturo Soria', 'Tecnologia', 'Si'); 
+insert into Solicitudes values (345, 'Instituto Torres Quevedo', 'Tecnologia', 'No'); 
+insert into Solicitudes values (345, 'Instituto Torres Quevedo', 'Ciencias', 'Si'); 
+insert into Solicitudes values (345, 'Instituto Torres Quevedo', 'Ciencias Sociales', 'No'); 
+insert into Solicitudes values (678, 'Instituto Ramiro de Maeztu', 'Ciencias Sociales', 'Si'); 
+insert into Solicitudes values (987, 'Instituto Ramiro de Maeztu', 'Tecnologia', 'Si'); 
+insert into Solicitudes values (987, 'Instituto San Isidro', 'Tecnologia', 'Si'); 
+insert into Solicitudes values (876, 'Instituto Ramiro de Maeztu', 'Tecnologia', 'No'); 
+insert into Solicitudes values (876, 'Instituto Arturo Soria', 'Ciencias', 'Si'); 
+insert into Solicitudes values (876, 'Instituto Arturo Soria', 'Ciencias Sociales', 'No'); 
+insert into Solicitudes values (765, 'Instituto Ramiro de Maeztu', 'Ciencias Sociales', 'Si'); 
+insert into Solicitudes values (765, 'Instituto Torres Quevedo', 'Ciencias Sociales', 'No'); 
+insert into Solicitudes values (765, 'Instituto Torres Quevedo', 'Ciencias', 'Si'); 
+insert into Solicitudes values (543, 'Instituto Arturo Soria', 'Tecnologia', 'No');
