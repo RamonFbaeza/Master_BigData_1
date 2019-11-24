16cde128ac077b24f61b74480668facab38abc4d
Carlos Emilio Rabazo Márquez <carlos.rabazo@gmail.com>
2019-11-24 21:00:46 +0100

    Update Institutos.sql

--- Institutos.sql
+++ Institutos.sql
@@ -2,3 +2,4 @@ CREATE TABLE Institutos(Nombre_Inst CHAR(35), Area CHAR(35), Plazas INTEGER, PRI
 CREATE TABLE Estudiantes(ID INTEGER, Nombre_Est CHAR(35), Puntos REAL, Valor INTEGER,PRIMARY KEY ( ID ),UNIQUE ( ID ) );
 CREATE TABLE Solicitudes(ID INTEGER, Nombre_Inst CHAR(35), Via CHAR(35), Decision CHAR(35),PRIMARY KEY ( ID, Nombre_Inst, Via ),
 FOREIGN KEY ( ID ) REFERENCES Estudiantes ( ID ), FOREIGN KEY ( Nombre_Inst ) REFERENCES Institutos ( Nombre_Inst ), UNIQUE ( ID, Nombre_Inst, Via ) );
+# Este comentario es un cambio que hemos hecho en el fichero SQL desde la Rama de Prueba
